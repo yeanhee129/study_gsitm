8f2d9784af769f3280298746f1c10c54effa9ae5
yeanhee129 <godqhr12392@gmail.com>
2024-08-27 17:55:50 +0900

    Create Customer.vue

---
+++ Customer.vue
@@ -0,0 +1,23 @@
+<template>
+    <template v-for="Customer in CustomerData" :key="Customer">
+        <p v-if="Customer.cus_id==1">
+            <input name="customerName" type="radio" checked> {{ Customer.cus_name }}
+        </p>
+        <p v-else>
+            <input name="customerName" type="radio"> {{ Customer.cus_name }}
+        </p>
+  </template>
+</template>
+  <script>
+  
+  export default {
+      name : 'CustomerVue',
+      props : {
+          CustomerData: Array,
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
