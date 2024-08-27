b0bac5b8fda71764acf222a328c556cc428f373a
yeanhee129 <godqhr12392@gmail.com>
2024-08-27 18:04:26 +0900

    Update Customer.vue

--- Customer.vue
+++ Customer.vue
@@ -1,12 +1,12 @@
 <template>
-    <template v-for="Customer in CustomerData" :key="Customer">
+    <div v-for="Customer in CustomerData" :key="Customer">
         <p v-if="Customer.cus_id==1">
             <input name="customerName" type="radio" checked> {{ Customer.cus_name }}
         </p>
         <p v-else>
             <input name="customerName" type="radio"> {{ Customer.cus_name }}
         </p>
-  </template>
+  </div>
 </template>
   <script>
   
