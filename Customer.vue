cfb8731f025c56aa2f27d69a2a3e5c8c2de912d8
yeanhee129 <godqhr12392@gmail.com>
2024-08-28 12:03:36 +0900

    Update Customer.vue

--- Customer.vue
+++ Customer.vue
@@ -1,23 +1,22 @@
 <template>
-    <div v-for="Customer in CustomerData" :key="Customer">
+    <div v-for="Customer in CustomerData" :key="Customer" >
         <p v-if="Customer.cus_id==1">
-            <input name="customerName" type="radio" checked> {{ Customer.cus_name }}
+            <input type="radio" name="customerName" @change="$emit('radioChange', $event.target.value)" :value="Customer.cus_id" checked> {{ Customer.cus_name }}
         </p>
         <p v-else>
-            <input name="customerName" type="radio"> {{ Customer.cus_name }}
+            <input type="radio" name="customerName" @change="$emit('radioChange', $event.target.value)" :value="Customer.cus_id"> {{ Customer.cus_name }}
         </p>
-  </div>
+    </div>
 </template>
   <script>
-  
   export default {
       name : 'CustomerVue',
+      
       props : {
           CustomerData: Array,
-      }
+      },
   }
   </script>
-  
-  <style>
-  
+
+  <style>  
   </style>
