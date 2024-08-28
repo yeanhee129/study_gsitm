001bc9389b030283a439771956fd30c4ded13c74
yeanhee129 <godqhr12392@gmail.com>
2024-08-28 12:03:52 +0900

    Create Manager.vue

---
+++ Manager.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-for="manager in ManagerData" :key="manager">
+        <ul v-if="m_id==manager.m_id">
+            <li>
+            <label for="manager">관리담당자</label>
+            <input type="text" id="manager" v-model="manager.m_name" Disabled="true">
+            <button class="button_img"></button>
+            </li>
+            <li>
+            <label for="department">**부서</label>
+            <input type="text" id="department" v-model="manager.dept" Disabled="true">
+            </li>
+            <li>
+            <label for="position">**직위</label>
+            <input type="text" id="position" v-model="manager.position" Disabled="true">
+            </li>
+            <li>
+            <label for="number2">**연락처</label>
+            <input type="text" id="number2" v-model="manager.phone" Disabled="true">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import ManagerData from '../assets/manager.js';
+
+export default {
+    name : 'ManagerVue',
+
+    data() {
+      return {
+        ManagerData: ManagerData,
+      }
+    },
+    
+    props : {
+        m_id : Number,
+    },
+}
+</script>
+
+<style>
+
+</style>
