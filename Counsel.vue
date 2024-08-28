bb4c1badac5a23a3ff3e7457c4d19ed3b7cf7512
yeanhee129 <godqhr12392@gmail.com>
2024-08-28 12:01:36 +0900

    Create Counsel.vue

---
+++ Counsel.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="detail_box">
+        <div v-for="counsel in CounselData" :key="counsel">
+            <p v-if="cus_id==counsel.cus_id">
+              {{ counsel.date + ' ' + counsel.time + ' ' + counsel.content }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+import CounselData from '../assets/counsel.js';
+
+export default {
+    name : 'ConselVue',
+
+    data() {
+      return {
+        CounselData: CounselData,
+      }
+    },
+
+    props : {
+        cus_id : Number,
+    },
+}
+</script>
+
+<style>
+
+</style>
